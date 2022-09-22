226dbbf25605bafdabefd287e10f484a1f4336cb
Troy <wilson.troy.anthony@gmail.com>
2022-09-21 20:14:41 -0400

    Added ProductController

---
+++ ProductsMvcDbFirst/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsMvcDbFirst.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // save to database
+        public IActionResult AddProductToDb() 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
